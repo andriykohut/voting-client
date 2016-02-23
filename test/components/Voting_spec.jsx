b68517971ab5b8ae15034a26c3af13bbdb990737
Andriy Kogut <kogut.andriy@gmail.com>
2016-02-23 18:57:12 +0200

    callback test

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -7,14 +7,30 @@ import {expect} from 'chai';
 describe('voting', () => {
 
   it('renders a pair of buttons', () => {
-    const component = TestUtils.renderIntoDocument(
+    const renderer = TestUtils.createRenderer();
+    renderer.render(
       <Voting pair={['Trainspotting', '28 Days Later']} />
     );
-    const buttons = TestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+    const component = renderer.getRenderOutput();
+    const buttons = component.props.children;
 
     expect(buttons.length).to.equal(2);
-    expect(buttons[0].textContent).to.equal('Trainspotting');
-    expect(buttons[1].textContent).to.equal('28 Days Later');
+    expect(buttons[0].key).to.equal('Trainspotting');
+    expect(buttons[1].key).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = TestUtils.renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              vote={vote} />
+    );
+    const buttons = TestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+    TestUtils.Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 
 });
